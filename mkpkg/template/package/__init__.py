509b3537c5419887db258778017a168728b075f5
Julian Berman <Julian@GrayVines.com>
2019-10-03 15:36:21 -0400

    pkg_resources -> importlib_metadata
    The latter is basically the newer replacement hooray.
    
    Closes: #37.

--- mkpkg/template/package/__init__.py
+++ mkpkg/template/package/__init__.py
@@ -1,5 +1,2 @@
-from pkg_resources import get_distribution, DistributionNotFound
-try:
-    __version__ = get_distribution(__name__).version
-except DistributionNotFound:  # pragma: no cover
-    pass
+import importlib_metadata
+__version__ = importlib_metadata.version(__name__)
